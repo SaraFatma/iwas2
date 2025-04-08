d405aa179151a30ac4593872f18c1625f92f3ad2
SaraFatma <141361121+SaraFatma@users.noreply.github.com>
2025-04-08 19:20:42 +0530

    html pages and folder structure

---
+++ src/main/java/com/example/iwas/IwasApplication.java
@@ -0,0 +1,13 @@
+package com.example.iwas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IwasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IwasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/iwas/IwasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iwas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IwasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
